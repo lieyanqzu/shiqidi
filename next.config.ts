fed074b671491142d6785ac4f73cf9eeb5307ec1
lieyanqzu <lieyanqzu@hotmail.com>
2024-12-03 00:31:06 +0800

    开发环境跳过检查

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,12 @@ const nextConfig: NextConfig = {
   env: {
     NEXT_PUBLIC_ICP_NUMBER: process.env.ICP_NUMBER,
   },
+  eslint: {
+    ignoreDuringBuilds: process.env.APP_ENV === 'development',
+  },
+  typescript: {
+    ignoreBuildErrors: process.env.APP_ENV === 'development',
+  },
 };
 
 export default nextConfig;
