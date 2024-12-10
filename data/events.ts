92e411fd35e3f4d7e39091d3ffdbf47c14a5e2df
lieyanqzu <lieyanqzu@hotmail.com>
2024-12-11 01:44:59 +0800

    更新活动日历

--- data/events.ts
+++ data/events.ts
@@ -47,6 +47,13 @@ export const events: Event[] = [
     endTime: new Date('2025-01-02T14:00:00-08:00'),
     format: '现开',
   },
+  {
+    type: 'midweek_magic',
+    title: '先驱大师幻影现开',
+    startTime: new Date('2024-01-07T14:00:00-08:00'),
+    endTime: new Date('2025-01-09T14:00:00-08:00'),
+    format: '现开',
+  },
   // 竞技轮抽
   {
     type: 'premier_draft',
@@ -132,6 +139,24 @@ export const events: Event[] = [
     endTime: new Date('2024-12-22T16:00:00-08:00'),
     format: '万智牌：基石构筑限制赛',
     description: '两日赛事，获胜者将获得参加即将到来的竞技场锦标赛的资格。',
+  },
+
+  // Arena Open
+  {
+    type: 'arena_open',
+    title: '竞技场公开赛 - 先驱大师 奖励列表：献力 - 第一天',
+    startTime: new Date('2024-01-11T06:00:00-08:00'),
+    endTime: new Date('2024-01-12T03:00:00-08:00'),
+    format: '先驱大师现开',
+    description: '第一天比赛采用现开赛制，可选择BO1或BO3模式参赛。',
+  },
+  {
+    type: 'arena_open',
+    title: '竞技场公开赛 - 先驱大师 奖励列表：献力 - 第二天',
+    startTime: new Date('2024-01-12T06:00:00-08:00'),
+    endTime: new Date('2024-01-12T08:00:00-08:00'),
+    format: '先驱大师轮抽',
+    description: '第二天比赛采用BO3轮抽赛制，仅限第一天晋级选手参加。',
   },
 
   // 竞技场锦标赛
