5da5efb3cc9c752f7583d977d0f7e9a4363d806a
lieyanqzu <lieyanqzu@hotmail.com>
2024-12-10 13:13:19 +0800

    更新活动日历

--- lib/store/card.ts
+++ lib/store/card.ts
@@ -22,7 +22,7 @@ export const useCardStore = create<CardStore>((set) => ({
   cards: [],
   chineseCards: {},
   params: {
-    expansion: 'FDN',
+    expansion: 'PIO',
     format: 'PremierDraft', 
     start_date: '2016-01-01',
     end_date: today,
