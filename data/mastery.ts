908698013d4ed7e20168b8f0d24e941cf891e9dc
lieyanqzu <lieyanqzu@hotmail.com>
2025-07-17 14:28:11 +0800

    更新虚空边域基础数据

--- data/mastery.ts
+++ data/mastery.ts
@@ -3,13 +3,13 @@
  */
 export const masteryConfig = {
   // 系列代号
-  setCode: 'FIN',
+  setCode: 'EOE',
   // 当前赛季开始日期（UTC）
-  startDate: '2025-06-10',
+  startDate: '2025-07-29',
   // 当前赛季结束日期（UTC）
-  endDate: '2025-07-29',
+  endDate: '2025-09-23',
   // 最高等级
-  maxLevel: 50,
+  maxLevel: 70,
   // 默认每日胜场数
   defaultDailyWins: 4,
   // 默认每周胜场数
